180beeb12380ec0c8cec58058b52151959baf69c
nickmangelo96 <nmanesis@uoguelph.ca>
2018-11-14 20:42:27 -0500

    Second Commit: 5. Restructuring and organisation of script. The original script was well organised, by splitting the code into different parts including data analysis and biodiversity. To improve upon that, I reordered some of the parts and piped them so that Data Analysis is composed of three parts, in the sequence of how the analysis was done: Data filtering, Biodiversity, and Phylogeny 6. In the data filtering part, the original subsetting selected for the BIN URI markercode which was a good option for the original dataset, however the genus Pseudomonas lacks BIN URIs, they are NA in the data. 7. Instead of grepping the data, I used the select function to extract our wanted columns and used processid instead of the lacking BIN uris. 8. Shalini's original code looked at the data structure thoroughly, from its class and length, to the frequency of NA values. To add to that, I decided to omit the NA values from the database. 9. Originally, the data was also subsetted by markercodes eg the COI-5P. The code worked perfectly, but it wasn't used in further analysis. 10. For the biodiversity part, I continued what Shalini originally wrote where she looked at the countries with the most barcode data and used that to visualise it in a world map representing the countries that had samples of Pseudomonas and the frequency of their samples. 11. Finally, for the phylogeny analysis I used the DNAstringset function that Shalini had employed, but with a slight modification to convert a sample of the original data set containing 1 sample from each country. I then created a pairwise distance dna matrix and plotted the clusters using IDclusters to illustrate phylogeny between all these samples.

--- Assignment1_partC_shalini suraj Bhan.R
+++ Assignment1_partC_shalini suraj Bhan.R
@@ -1,36 +1,66 @@
+<<<<<<< HEAD
 ##### Packages to Install #####
+=======
+#N To tidy up the script, I created a list of all the packages that the user is asked to install. 
+>>>>>>> 0e05fd4172b572555e990a191b4d045590da1a3f
+
 install.packages("tidyverse")
 install.packages("readr")
 install.packages("dplyr")
+<<<<<<< HEAD
 install.packages("fansi")
 install.packages("stringi")
 install.packages("ape")
 install.packages("RSQLite")
 install.packages("DECIPHER")
 install.packages("stringr")
+install.packages("vegan")
 source("https://bioconductor.org/biocLite.R")
 biocLite("msa")
 biocLite("Biostrings")
 abiocLite("muscle")
 biocLite("msa")
 biocLite("DECIPHER")
+install.packages('rworldmap')
+
+#### Importing the data file ####
+
+#N Loading selected data set of Daphnia specimen using Bold Api
+library(tidyverse) %>% bold_data <- read_tsv("http://www.boldsystems.org/index.php/API_Public/combined?taxon=Pseudomonas&format=tsv")
 
 ##### Data Analysis ####
 
+##### Part 1: Data Filtering ####
+
+library(stringi)
+library(ape)
+library(RSQLite)
+library(stringr)
+library(Biostrings)
+library(muscle)
+library(msa)
+library(DECIPHER)
+
+############### Checking data-sets
+
+=======
+
 ############### Importing the data file
 
-
+#N renamde data2 into bold_data for clarity 
 
 #bold_data <- read.delim(file.choose(), header = T)
 #bold_data
 
+#N to avoid having to download a data file and import it, we can simply use the read_tsv function and read one directly from a bold URL 
 
+#N Shalini's plan for her resarch question was to analyse gut microbe data. She did so using 3784 bold samples including 127 unique species of the mammalian class, including 9 orders: Artiodactyla Carnivora Cetacea Chiroptera Didelphimorphia Lagomorpha Perissodactyla Primates Rodentia Soricomorpha). For the improvements I picked Pseudomonas as the chosen genus for analysis to help narrow down the results and focus on the original research question, though we could substitute it for any other bacterium present in the gut flora. 
 
 library(tidyverse)
 
 bold_data <- read_tsv("http://www.boldsystems.org/index.php/API_Public/combined?taxon=Pseudomonas&format=tsv") 
 
-#N Loading selected data set of Daphnia specimen using Bold Api
+#NLoading selected data set of Daphnia specimen using Bold Api
 
 
 #N The Data set checking and analysis works well but coild be expanded upon
@@ -39,6 +69,7 @@ bold_data <- read_tsv("http://www.boldsystems.org/index.php/API_Public/combined?
 ############### Checking data-sets
 
 ############### Class and summary
+>>>>>>> 0e05fd4172b572555e990a191b4d045590da1a3f
 class(bold_data) #tbl data.frame
 summary(bold_data) #540 entries
 str(bold_data)
@@ -50,6 +81,50 @@ names(bold_data)
 
 length(unique(bold_data$species_name)) #40 
 
+<<<<<<< HEAD
+#Using the select function instead of grepping 
+
+#N looking at NA values
+
+sum(is.na(bold_data$bin_uri))
+
+
+#N selecting data 
+
+library(dplyr)
+bold_data %>%
+  select(processid, species_name, nucleotides, country) -> bold_data2 #select function to extract wanted marker columns
+
+length(is.na(bold_data2)) #identify NA values
+
+bold_data2 <- na.omit(bold_data2) #N Option to remove NA data
+
+############## checking unique markers
+
+unique(bold_data$markercode)
+
+############### Database, with list of markercodes
+
+MarkerCodeList <- bold_data %>%
+  group_by(markercode) %>%
+  summarize(n = length(processid)) %>%
+  arrange(desc(n)) %>%
+  print()
+
+############### Subseting the dataframe bold_data to retain those records having a COI-5P markercode.
+
+DataCOI <- bold_data %>%
+  filter(markercode == "COI-5P") %>%
+  filter(str_detect('nucleotides', "[ACGT]"))
+DataCOI
+
+
+##### Part 2 Biodiversity ####
+
+#N Country with most barcode data gives top 10
+
+bold_data2 %>%
+=======
 #N The BIN URI markercode was a good option for the original dataset, however the genus Pseudomonas lacks BIN URIs, they are NA in the data. As an alternative we can pick 
 
 ############### No. of BINs 
@@ -80,15 +155,65 @@ bold_data %>%
 
 ############## Country with most barcode data 
 
-
+install.packages("fansi")
 library(fansi)
 
 bold_data %>%
+>>>>>>> 0e05fd4172b572555e990a191b4d045590da1a3f
   group_by(country) %>%
   summarize(count = length(processid)) %>%
   arrange(desc(count))
 
 
+<<<<<<< HEAD
+#N Possible visualisation
+
+library(rworldmap)
+bold_dataframe <- as.data.frame(bold_data2)
+countries <- as.data.frame(table(bold_dataframe$country))
+colnames(countries) <- c("country", "value")
+matched <- joinCountryData2Map(countries, joinCode="NAME", nameJoinColumn="country") #matching countries from data to those in the Rworldmap package: 26 matched, 217 did not 
+mapCountryData(matched, nameColumnToPlot="value", mapTitle="Pseudomonas samples per country ", catMethod = "pretty", colourPalette = "white2Black")
+
+library(vegan)
+
+############## changing the data set format
+
+library(dplyr)
+library(magrittr)
+library(tidyverse)
+
+
+
+##### Part 3 Phylogeny ####
+
+library(dplyr); bold_data2 %>% group_by(country) %>% sample_n(1) -> bold_sample #sample data by countries, to reduce dataset 
+
+
+
+#N Converted to DNA string but no further analysis performed 
+
+############## Converting to data string
+
+bold_data_string <- DNAStringSet(bold_sample$nucleotides) #N No NA values accepted
+
+
+#NPicking up from where the anaylsis left off using the DNA stringset to perform a muscle alignment 
+#N create dendrogram distance matrix
+#N Specaccum for biodiversity 
+
+
+bold_data_alignment <- DNAStringSet(muscle::muscle(bold_data_string, maxiters = 2,diags = TRUE)) #running muscle MSA 
+
+
+bold_data_DNAbin<- as.DNAbin(bold_data_alignment)
+
+distanceMatrix1 <- dist.dna(bold_data_DNAbin, model = "TN93", as.matrix = TRUE, pairwise.deletion = TRUE) #creating a a pairwise distance matrix using the dist.dna function and selecting the Tamura-Nei 93 (TN93) model which offers a substitution model of nucleotide evolution
+
+clusters.Daphnia16S <- IdClusters(distanceMatrix1,method = "single", cutoff= 0.02, showPlot = TRUE, type = "both", verbose = TRUE) #plotting the clusters using IDclusters creating a distnace similarity matrix at a 0.02 cutoff to provide enough clusters without overdoing it. 
+
+
+=======
 #United States 54
 
 
@@ -121,12 +246,25 @@ unique(bold_data$markercode)
 
 
 
-
-
+install.packages("stringi")
 library(stringi)
+
+install.packages("ape")
 library(ape)
+
+install.packages("RSQLite")
 library(RSQLite)
+
+install.packages("DECIPHER")
+install.packages("stringr")
 library(stringr)
+
+source("https://bioconductor.org/biocLite.R")
+biocLite("msa")
+biocLite("Biostrings")
+abiocLite("muscle")
+biocLite("msa")
+biocLite("DECIPHER")
 library(Biostrings)
 library(muscle)
 library(msa)
@@ -162,3 +300,4 @@ class(DataCOI$nucleotides)
 ######## Converting to 18s markerset
 Data18snucleotides <- DNAStringSet(DataCOI$nucleotides)
 Data18snucleotides
+>>>>>>> 0e05fd4172b572555e990a191b4d045590da1a3f
